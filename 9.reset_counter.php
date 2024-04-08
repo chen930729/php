e5cd08ed6e0a4e546880d60568505fcc5e71f96f
chen930729 <135223972+chen930729@users.noreply.github.com>
2024-04-08 17:47:03 +0800

    Create 9.reset_counter.php

---
+++ 9.reset_counter.php
@@ -0,0 +1,14 @@
+<?php
+    session_start();
+    unset($_SESSION["counter"]);
+    echo "counter重置成功....";
+    echo "<meta http-equiv=REFRESH content='2; url=8.counter.php'>";
+
+?>
+
+
+#刪除 Session 中名為 "counter" 的變數  
+  #unset($_SESSION['變數'])：刪除Session變數
+  #session_unset() 或者session_destroy()也可以刪除Session變數
+#顯示成功消息
+#使用 <meta> 在 HTML中設置重定向,將在2秒後導向到 8.counter.php 頁面
