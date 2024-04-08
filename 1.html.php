61d60b9cb0ce1337eefae2e762a8bf6f9acf75eb
chen930729 <135223972+chen930729@users.noreply.github.com>
2024-04-08 15:53:40 +0800

    Update 1.html.php

--- 1.html.php
+++ 1.html.php
@@ -10,7 +10,7 @@
 </html>
 
 
-//結合了 HTML 和 PHP 的簡單網頁
-//在 <body> 中，用了三次 php 來輸出 "Hello world!"
-//<h1> 為標題
-//html 部分是用來排版內容的
+#結合了 HTML 和 PHP 的簡單網頁
+#在 <body> 中，用了三次 php 來輸出 "Hello world!"
+#<h1> 為標題
+#html 部分是用來排版內容的
