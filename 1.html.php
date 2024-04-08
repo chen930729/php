776e6794febbea8fd6b10c277c4ebbfb54baed8a
chen930729 <135223972+chen930729@users.noreply.github.com>
2024-04-08 15:39:30 +0800

    Create 1.html.php

---
+++ 1.html.php
@@ -0,0 +1,16 @@
+<html>
+    <head><title>html+php</title></head>      
+    <body>
+        <?php echo "Hello world!"; ?> 
+        <h1>html</h1>
+        <?php echo "Hello world!"; ?> 
+        <h1>html</h1>
+        <?php echo "Hello world!"; ?> 
+    </body>
+</html>
+
+
+結合了 HTML 和 PHP 的簡單網頁
+在 <body> 中，用了三次 php 來輸出 "Hello world!"
+<h1> 為標題
+html 部分是用來排版內容的
