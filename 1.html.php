4d2767e8b2ae2ad14afc67a4b9fc71177379979f
chen930729 <135223972+chen930729@users.noreply.github.com>
2024-04-08 16:09:17 +0800

    Update 1.html.php

--- 1.html.php
+++ 1.html.php
@@ -11,6 +11,10 @@
 
 
 #結合了 HTML 和 PHP 的簡單網頁
-#在 <body> 中，用了三次 php 來輸出 "Hello world!"
+#PHP屬於直譯式語言，可以直接內嵌於網頁當中
+#使用 <? 與 ?> 加在HTML程式語言
+#php每段程式，需以 ; 作為結尾
+#echo：列印
+#php echo輸出 "Hello world!"
 #<h1> 為標題
 #html 部分是用來排版內容的
