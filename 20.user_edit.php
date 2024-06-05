c9659b24231ddac01cbc42054a350cd5f96abb91
chen930729 <135223972+chen930729@users.noreply.github.com>
2024-06-05 10:22:12 +0800

    Create 20.user_edit.php

---
+++ 20.user_edit.php
@@ -0,0 +1,22 @@
+<?php
+
+    # 禁止顯示所有錯誤報告
+    error_reporting(0);
+    # 啟動 PHP 的 Session
+    session_start();
+    if (!$_SESSION["id"]) {
+        echo "請登入帳號";
+        echo "<meta http-equiv=REFRESH content='3; url=2.login.html'>";
+    }
+    else{   
+        $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
+        if (!mysqli_query($conn, "update user set pwd='{$_POST['pwd']}' where id='{$_POST['id']}'")){
+            echo "修改錯誤";
+            echo "<meta http-equiv=REFRESH content='3; url=18.user.php'>";
+        }else{
+            echo "修改成功，三秒鐘後回到網頁";
+            echo "<meta http-equiv=REFRESH content='3; url=18.user.php'>";
+        }
+    }
+
+?>
