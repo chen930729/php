b72909b76499e4237d55e69c7e235aea96c48e74
chen930729 <135223972+chen930729@users.noreply.github.com>
2024-06-05 10:24:15 +0800

    Create 23.bulletin_add.php

---
+++ 23.bulletin_add.php
@@ -0,0 +1,22 @@
+<?php
+    # 禁止顯示所有錯誤報告
+    error_reporting(0);
+    # 啟動 PHP 的 Session
+    session_start();
+    if (!$_SESSION["id"]) {
+        echo "please login first";
+        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
+    }
+    else{
+        $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
+        $sql="insert into bulletin(title, content, type, time) 
+        values('{$_POST['title']}','{$_POST['content']}', {$_POST['type']},'{$_POST['time']}')";
+        if (!mysqli_query($conn, $sql)){
+            echo "新增命令錯誤";
+        }
+        else{
+            echo "新增佈告成功，三秒鐘後回到網頁";
+            echo "<meta http-equiv=REFRESH content='3, url=11.bulletin.php'>";
+        }
+    }
+?>
