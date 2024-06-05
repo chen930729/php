fbb3f39bb83ce09f95aa785811e73edbe313190e
chen930729 <135223972+chen930729@users.noreply.github.com>
2024-06-05 10:03:27 +0800

    Update 12.logout.php

--- 12.logout.php
+++ 12.logout.php
@@ -1,9 +1,15 @@
 <?php
+    // 啟動 PHP 的 Session
     session_start();
-    unset($_SESSION["id"]);
-    echo "登出成功....";
-    echo "<meta http-equiv=REFRESH content='3; url=2.login.html'>";
 
+    // 刪除會話變量 "id"，用來標識用戶已登出
+    unset($_SESSION["id"]);
+
+    // 輸出登出成功的訊息
+    echo "登出成功....";
+
+    // 使用 HTML meta 標籤設置 3 秒後重定向到登入頁面 2.login.html
+    echo "<meta http-equiv=REFRESH content='3; url=2.login.html'>";
 ?>
 
 登出功能
